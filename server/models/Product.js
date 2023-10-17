514b355d1ac94bad3fb9a1f51827ffda28f7d71f
HajarMIDANY <hajar2001midany@gmail.com>
2023-10-17 10:41:52 +0100

    product model

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    id:{type: String},
+    sku:{type: String},
+    product_image:{type: String},
+    product_name:{type: String},
+    subcategory_id:{type: String},
+    short_description:{type: Text},
+    long_description:{type: Text},
+    price:{type: Number},
+    discount_price:{type: Number},
+    options:{type: Blob,Array},
+    active:{type:Boolean}
+}
+)
+
+module.exports = mongoose.model('Product', productSchema)
