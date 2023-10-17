8c7f24bba087870198ae35b8bba5bcc1fef870be
oussamaom7 <om7.oussama@gmail.com>
2023-10-17 17:00:51 +0200

    modify router

--- server/models/Categorie.js
+++ server/models/Categorie.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 const categorieSchema = new mongoose.Schema({
-    category_name: {type: string},
-    active: {type: boolean}
+    category_name: {type: string,
+                     required: true,
+                     unique: true},
+    active: {type: boolean,
+        default: false,
+         required:true}
 });
 
 module.exports = mongoose.model('categorie', categorieSchema);
--- server/models/Subcategorie.js
+++ server/models/Subcategorie.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+
+const subcategorySchema = new mongoose.Schema({
+    subcategory_name: {type: string,
+    required: true,
+    unique: true},
+    category_id: {type: string,
+    required: true},
+    active: {type: boolean,
+    require: true,
+    default: false}
+});
+
+module.exports = mongoose.model('subcategory', subcategorySchema);
