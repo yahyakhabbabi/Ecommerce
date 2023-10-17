5ccc3befff1e8241eb8dbef4bfa6ae02012fafe9
oussamaom7 <om7.oussama@gmail.com>
2023-10-17 15:06:54 +0200

    salam

--- server/models/Categorie.js
+++ server/models/Categorie.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+const categorieSchema = new mongoose.Schema({
+    category_name: {type: string},
+    active: {type: boolean}
+});
+
+module.exports = mongoose.model('categorie', categorieSchema);
