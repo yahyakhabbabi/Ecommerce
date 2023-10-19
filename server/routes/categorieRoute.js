408fd72c8b5f8b4667cf85894f4c92361004d968
yahya <khabbabiyahia@gmail.com>
2023-10-19 11:53:48 +0100

    dernier version of category-name

--- server/routes/categorieRoute.js
+++ server/routes/categorieRoute.js
@@ -6,8 +6,13 @@ const categorieController = require('../controllers/categorieController');
 
 
 router.post('/',categorieController.creatCategorie);
-router.get('/',categorieController.listCategories);
-router.get('/',categorieController.searchCategories);
+router.get('/',(req,res,next)=>{
+    if(Object.keys(req.query).length>0){
+        categorieController.searchCategories(req, res, next);
+    }else{
+        categorieController.listCategories(req, res, next);
+    }
+});
 router.get('/:id',categorieController.idCategories);
 router.put('/:id',categorieController.updateCategories);
 router.delete('/:id',categorieController.deleteCategories);
