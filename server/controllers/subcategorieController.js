b88de6cf7da0cbe268a48219773b59f15b217a6e
oussamaom7 <om7.oussama@gmail.com>
2023-10-20 17:20:36 +0200

    incomming

--- server/controllers/subcategorieController.js
+++ server/controllers/subcategorieController.js
@@ -1,3 +1,4 @@
+const { categorie } = require('../models/Categorie');
 const {subcategorie} = require('../models/Subcategorie');
 
 
@@ -16,16 +17,81 @@ exports.creatSubcategorie = async function (req,res){
         res.status(500).send({ status: 500, message: 'Internal server error' });
     }
     }
-    
-}
+
 exports.listSubcategories = async function (req,res){
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const perPage = 10;
+        const startIndex = (page - 1) * perPage;
 
-}
+        const subcategories = await subcategorie.aggregate([
+            {
+                $lookup: {
+                  from: 'categories', 
+                  localField: 'category',
+                  foreignField: '_id',
+                  as: 'category',
+                },
+              },
+              {
+                $unwind: '$category',
+              },
+              {
+                $skip: startIndex,
+              },
+              {
+                $limit: perPage,
+              },
+            ]);
+            return res.status(200).send(subcategorie)
+    } catch (error) {
+        res.status(500).send({ error: 'Server error' });
+    }
+    
+
+
+    }
+
+
 exports.searchSubcategories = async function (req,res){
+    try {
+        const query = req.query.query || '';
+        const page = parseInt(req.query.page) || 1;
+        const perPage = 10;
+        const startIndex = (page - 1) * perPage;
 
-}
+        const subcategories = await subcategorie.aggregate([
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "categorie",
+                    foreignField: "_id",
+                    as: "categotrie"
+                },
+            },
+            {
+                $unwind: "$categorie",
+            },
+            {
+                $match: {
+                    subcategoryName: { $regex: query, $options: 'i' },
+                },
+            },
+            {
+                $skip: startIndex,
+              },
+              {
+                $limit: perPage,
+              },
+            ]);
+
+            res.status(200).json(subcategories);
+    } catch (error) {
+      res.status(500).json({ status: 500, message: 'Internal server error' });
+    }
+  };
 exports.idSubcategories = async function (req,res){
-
+  
 }
 exports.updateSubcategories = async function (req,res){
 
