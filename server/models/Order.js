6a49c20bc924805f7e0539339934c3c89c2f0f09
yahya <khabbabiyahia@gmail.com>
2023-10-21 18:01:40 +0100

    dev is work

--- server/models/Order.js
+++ server/models/Order.js
@@ -10,4 +10,4 @@ const orderSchema = new mongoose.Schema({
     }
 });
 
-module.exports = {Order:mongoose.model('Order', orderSchema)}; 
+module.exports = {Orders:mongoose.model('Orders', orderSchema)}; 
